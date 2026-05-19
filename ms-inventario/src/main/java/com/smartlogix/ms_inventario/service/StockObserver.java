08998327ff171d4acfad72de4c55a071d53b678f
Alexis-Aranguiz <alex.aranguiz@duocuc.cl>
2026-05-19 13:51:56 -0400

    Update StockObserver.java

--- ms-inventario/src/main/java/com/smartlogix/ms_inventario/service/StockObserver.java
+++ ms-inventario/src/main/java/com/smartlogix/ms_inventario/service/StockObserver.java
@@ -3,14 +3,14 @@ package com.smartlogix.ms_inventario.service;
 public interface StockObserver {
 
     /**
-     * update() del GoF
+     * update() 
      * El Publisher llama este método en cada suscriptor registrado
      * cuando el stock cambia.
      *
-     * @param idProducto     ID del producto afectado
-     * @param nombreProducto Nombre del producto
-     * @param tipoAlerta     BAJO, SOBRESTOCK, AGOTADO, NORMAL
-     * @param stockNuevo     Nuevo valor de stock
+      @param idProducto     ID del producto afectado
+      @param nombreProducto Nombre del producto
+      @param tipoAlerta     BAJO, SOBRESTOCK, AGOTADO, NORMAL
+      @param stockNuevo     Nuevo valor de stock
      */
     void update(Long idProducto, String nombreProducto,
                 String tipoAlerta, Integer stockNuevo);
