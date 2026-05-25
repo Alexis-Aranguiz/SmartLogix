e4276750f26e103abd76a411047fdcc40849593a
MiguelAllende01 <migu.allende@duocuc.cl>
2026-05-24 21:51:49 -0400

    Update StockObserver.java

--- ms-inventario/src/main/java/com/smartlogix/ms_inventario/service/StockObserver.java
+++ ms-inventario/src/main/java/com/smartlogix/ms_inventario/service/StockObserver.java
@@ -18,7 +18,6 @@ public interface StockObserver {
      *
      * @param idProducto     ID del producto afectado
      * @param nombreProducto Nombre del producto
-     * @param tipoAlerta     BAJO, SOBRESTOCK, AGOTADO, NORMAL
      * @param stockNuevo     Nuevo valor de stock
      */
     void update(Long idProducto, String nombreProducto,
