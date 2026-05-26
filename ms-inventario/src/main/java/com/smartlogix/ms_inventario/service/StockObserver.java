88a62660ddef7cc42c8930762c4e83578bbdb388
Alexis-Aranguiz <alex.aranguiz@duocuc.cl>
2026-05-26 12:50:06 -0400

    coments

--- ms-inventario/src/main/java/com/smartlogix/ms_inventario/service/StockObserver.java
+++ ms-inventario/src/main/java/com/smartlogix/ms_inventario/service/StockObserver.java
@@ -1,18 +1,9 @@
 package com.smartlogix.ms_inventario.service;
 
-/**
- * PATRÓN OBSERVER — Interfaz Subscriber (GoF)
- * ══════════════════════════════════════════
- * Equivale a «interface» EventListener del diagrama del profe.
- * Todo Observer concreto debe implementar el método update().
- *
- * Publisher:  ProductoService (notifica cambios de stock)
- * Subscriber: Cualquier clase que implemente esta interfaz
- */
 public interface StockObserver {
 
     /**
-     * update() del GoF
+     * update()
      * El Publisher llama este método en cada suscriptor registrado
      * cuando el stock cambia.
      *
